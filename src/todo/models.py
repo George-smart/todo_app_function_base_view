44d8ce46a7372070744ad7ec7313589f681b19bd
George-smart <georgechinedu2018@gmail.com>
2022-07-21 13:57:34 +0100

    Rendered all my Html Template in the views and also created my Database Model

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
-# Create your models here.
+User = get_user_model()
+
+class TodoApp(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    created_date = models.DateTimeField(default=datetime.now)
+    is_complete = models.BooleanField(default=False)
+    due_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import index, signup, signin
 
 urlpatterns = [
-
+    path('', index, name='home'),
+    path('signup', signup, name='signup'),
+    path('signin', signin, name='signin'),
 ]
