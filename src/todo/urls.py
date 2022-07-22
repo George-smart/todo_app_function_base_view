39c5664d7ab5bf1e652c902f8c37d4b842de11e8
George-smart <georgechinedu2018@gmail.com>
2022-07-22 23:35:38 +0100

    coding of Sign up and Signin functionalities

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, signup, signin
+from .views import index, signup, signin, signout
 
 urlpatterns = [
     path('', index, name='home'),
     path('signup', signup, name='signup'),
     path('signin', signin, name='signin'),
+    path('signout', signout, name='signout'),
 ]
